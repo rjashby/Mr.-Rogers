bd755f34539d1855dc7c2deb0b12167ef0565373
Ryan Ashby <rjapsu160@gmail.com>
2022-01-21 09:41:03 -0800

    pass third test, replacing 1s with 'Beep!'

--- js/scripts.js
+++ js/scripts.js
@@ -13,10 +13,13 @@ function rogers(number) {
   let initialArray = number.toString().split("");
   let responseArray = []
   initialArray.forEach(function(number) {
-    if (number === 1) {
+    if (number === "1") {
       responseArray.push("Beep!")
-    }
+    } else if (number !== "1") {
     responseArray.push(number);
+    console.log(typeof responseArray[0])
+    console.log(typeof responseArray[1])
+    }
   });
   return responseArray;
 }
