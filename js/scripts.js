fb0411efdfd084509ef2cf5de17b398b71921c3d
Ryan Ashby <rjapsu160@gmail.com>
2022-01-21 10:24:35 -0800

    pass fifth test, changing '3's to 'be my neigbor'

--- js/scripts.js
+++ js/scripts.js
@@ -17,6 +17,8 @@ function rogers(number) {
       responseArray.push("Beep!")
     } else if (number === "2") {
       responseArray.push("Boop!")
+    } else if (number === "3") {
+      responseArray.push("Won't you be my neighbor?")
     } else {
       responseArray.push(number);
     }
