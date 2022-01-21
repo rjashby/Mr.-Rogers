26eec9208f0f5263d325794cbddd2178d3930f93
Ryan Ashby <rjapsu160@gmail.com>
2022-01-21 11:25:07 -0800

    pass new test, number into array of all vals counting up to number

--- js/scripts.js
+++ js/scripts.js
@@ -9,26 +9,29 @@ $(document).ready(function() {
   });
 });
 
+
 function rogers(number) {
   const initialArray = [];
     for (let index = 0; index <= number; index ++) {
       initialArray.push(index)
     };
     console.log(initialArray);
-  const responseArray = []
-  initialArray.forEach(function(number) {
-    if (number === "1") {
-      responseArray.push("Beep!");
-    } else if (number === "2") {
-      responseArray.push("Boop!");
-    } else if (number === "3") {
-      responseArray.push("Won't you be my neighbor?");
-    } else {
-      responseArray.push(number);
-    }
-  });
-  // console.log(responseArray[12]);
-  // console.log(responseArray[20]);
-  // console.log(responseArray[32]);
-  return responseArray;
-}
+  }
+
+//     const responseArray = []
+//   initialArray.forEach(function(number) {
+//     if (number === "1") {
+//       responseArray.push("Beep!");
+//     } else if (number === "2") {
+//       responseArray.push("Boop!");
+//     } else if (number === "3") {
+//       responseArray.push("Won't you be my neighbor?");
+//     } else {
+//       responseArray.push(number);
+//     }
+//   });
+//   // console.log(responseArray[12]);
+//   // console.log(responseArray[20]);
+//   // console.log(responseArray[32]);
+//   return responseArray;
+// }
